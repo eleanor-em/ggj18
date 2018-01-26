5d111ca4ac645b404809ed860c575dbd96c490fe
Game Jam <gamejam@ad.aie.edu>
2018-01-27 09:55:31 +1100

    can shoot at stuff

--- GGj18/Assets/Scripts/CameraController.cs
+++ GGj18/Assets/Scripts/CameraController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public float rotSpeed = 0.1f;
     public GameObject targetObject;
+
+    private Vector3 initialForward;
     private Vector3 offset;
+    private Rigidbody targetRb;
 
     void Start() {
+        initialForward = transform.forward;
         offset = transform.position - targetObject.transform.position;
+        targetRb = targetObject.GetComponent<Rigidbody>();
     }
 
     void FixedUpdate() {
         transform.position = offset + targetObject.transform.position;
+        if (targetRb.velocity.sqrMagnitude > 0) {
+            //transform.forward = Vector3.Lerp(transform.forward, targetRb.velocity.normalized, rotSpeed);
+        } else {
+            transform.forward = Vector3.Lerp(transform.forward, initialForward, rotSpeed);
+        }
     }
 }
